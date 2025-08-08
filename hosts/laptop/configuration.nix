8bb258d963abb94afd6daa4b91b1280a263f6d9e
Andrei-Kharitonov <89729599+Andrei-Kharitonov@users.noreply.github.com>
2025-08-08 14:30:21 +0400

    laptop keyboard remap

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -35,6 +35,36 @@
 
   networking.hostName = "laptop";
 
+  # Keyboard remap
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            capslock = "leftcontrol";
+            rightalt = "esc";
+            kpenter = "capslock";
+            # numpad arrows
+            kp2 = "down";
+            kp4 = "left";
+            kp5 = "down";
+            kp6 = "right";
+            kp8 = "up";
+            # numpad navigation
+            kp7 = "home";
+            kp1 = "end";
+            kp9 = "pageup";
+            kp3 = "pagedown";
+            kp0 = "insert";
+            kpdot = "delete";
+          };
+        };
+      };
+    };
+  };
+
   # Don't change this!
   system.stateVersion = "25.05"; # Did you read the comment?
 }
